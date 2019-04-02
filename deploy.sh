e6bd522390617a61c4b5147e2b25f219aadcb473
Alex Bacchin <alexbacchin@hotmail.com>
2019-04-02 16:38:24 +1100

    added bastion host template

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 aws cloudformation deploy --template-file stacks/network.yml --stack-name plato-stack 
-aws cloudformation describe-stack-events --stack-name plato-stack 
+aws cloudformation deploy --template-file stacks/bastion-host.yml --stack-name plato-ssh --parameter-override NetworkVPCStack=plato-stack SSHKeyName=aws_id_rsa  --capabilities CAPABILITY_IAM
