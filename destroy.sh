837656ae6aea736dca4a81e482f46e8c453e2588
Alex Bacchin <alexbacchin@hotmail.com>
2019-04-04 06:48:02 +1100

    destroy script updated

--- destroy.sh
+++ destroy.sh
@@ -1,3 +1,6 @@
 aws cloudformation delete-stack --stack-name plato-lamp
+aws cloudformation wait stack-delete-complete --stack-name plato-lamp
 aws cloudformation delete-stack --stack-name plato-bastion
+aws cloudformation wait stack-delete-complete --stack-name plato-bastion
 aws cloudformation delete-stack --stack-name plato-network
+aws cloudformation wait stack-delete-complete --stack-name plato-network
